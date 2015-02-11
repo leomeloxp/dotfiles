6b8b56113ae0f3393eaa0401019f8ec9c4420a03
Leonardo Melo <leomeloxp@gmail.com>
2015-02-11 17:17:01 +0000

    Numbers do not show decimals anymore
    I don't really care for such precisions so displaying decimal values for
    battery, wifi strength and cpu temperature was removed.

--- i3/i3status.py
+++ i3/i3status.py
@@ -21,9 +21,9 @@ status.register("load",
 
 # Shows your CPU temperature, if you have a Intel CPU
 
-status.register("temp", 
-                format="{temp:.0f}°C",  
-                color="#00FF00")
+status.register("temp",
+                format="{temp:.0f}°C",
+                color="#0099CC")
 
 # The battery monitor has many formatting options, see README for details
 
@@ -35,13 +35,13 @@ status.register("temp",
 # This would also display a desktop notification (via dbus) if the percentage
 # goes below 5 percent while discharging. The block will also color RED.
 status.register("battery",
-                format="{status}:{percentage:.2f}% [{remaining:%E%hh:%Mm}]",
+                format="{status}{percentage:.0f}% [{remaining:%E%hh:%Mm}]",
                 alert=True,
                 alert_percentage=10,
                 status={
                     "DIS": "↓",
                     "CHR": "↑",
-                    "FULL": "=",
+                    "FULL": "✓",
                 },)
 # Shows the address and up/down state of eth0. If it is up the address is shown in
 # green (the default value of color_up) and the CIDR-address is shown
@@ -61,7 +61,7 @@ status.register("network",
 # Note: requires both netifaces and basiciw
 status.register("wireless",
                 interface="wlp2s0",
-                format_up="{essid} {quality:03.0f}%",)
+                format_up="{essid} {quality:02.0f}%",)
 
 # Shows disk available space
 # status.register("disk",
